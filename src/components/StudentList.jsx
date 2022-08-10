d3178b46295030a7e093560c3713ef479428c583
sifat-jaber <sifatjaber@gmail.com>
2022-08-10 18:29:05 +0600

    initiated project

---
+++ src/components/StudentList.jsx
@@ -0,0 +1,14 @@
+
+
+const StudentList = () => {
+
+    return (
+        <div>
+            <h2 className="text-xl text-blue-600">Student List</h2>
+        </div>
+    );
+
+}
+
+
+export default StudentList;
